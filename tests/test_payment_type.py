95f2678e342b255b08465335224b80b3174faa01
Jubjub422 <ross.stovenour@gmail.com>
2022-03-03 11:29:09 -0600

    payment type tests running, create and delete

--- tests/test_payment_type.py
+++ tests/test_payment_type.py
@@ -1,39 +1,55 @@
-# from faker import Faker
-# from rest_framework import status
-# from rest_framework.test import APITestCase
-# from rest_framework.authtoken.models import Token
-# from django.core.management import call_command
-# from django.contrib.auth.models import User
+from faker import Faker
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from django.core.management import call_command
+from django.contrib.auth.models import User
+
+from bangazon_api.models.payment_type import PaymentType
 
 
-# class PaymentTests(APITestCase):
-#     def setUp(self):
-#         """
-#         Seed the database
-#         """
-#         call_command('seed_db', user_count=1)
-#         self.user1 = User.objects.filter(store=None).first()
-#         self.token = Token.objects.get(user=self.user1)
+class PaymentTests(APITestCase):
+    def setUp(self):
+        """
+        Seed the database
+        """
+        call_command('seed_db', user_count=2)
+        self.user1 = User.objects.filter(store=None).first()
+        self.token = Token.objects.get(user=self.user1)
 
-#         self.client.credentials(
-#             HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f'Token {self.token.key}')
 
-#         self.faker = Faker()
+        self.faker = Faker()
 
 
-#     def test_create_payment_type(self):
-#         """
-#         Ensure we can add a payment type for a customer.
-#         """
-#         # Add product to order
-#         data = {
-#             "merchant": self.faker.credit_card_provider(),
-#             "acctNumber": self.faker.credit_card_number()
-#         }
+    def test_create_payment_type(self):
+        """
+        Ensure we can add a payment type for a customer.
+        """
+        data = {
+            "merchant": self.faker.credit_card_provider(),
+            "acctNumber": self.faker.credit_card_number()
+        }
 
-#         response = self.client.post('/api/payment-types', data, format='json')
+        response = self.client.post('/api/payment-types', data, format='json')
 
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertIsNotNone(response.data['id'])
-#         self.assertEqual(response.data["merchant_name"], data['merchant'])
-#         self.assertEqual(response.data["acct_number"], data['acctNumber'])
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIsNotNone(response.data['id'])
+        self.assertEqual(response.data["merchant_name"], data['merchant'])
+        self.assertEqual(response.data["obscured_num"][-4:], data['acctNumber'][-4:])
+    
+    def test_delete_payment_type(self):
+        """
+        Ensure we can delete a payment type
+        """
+        payment_type = PaymentType()
+        
+        payment_type.customer = self.user1
+        payment_type.merchant_name = self.faker.credit_card_provider()
+        payment_type.acct_number = self.faker.credit_card_number()
+        
+        payment_type.save()
+        url= f'/api/payment-types/{payment_type.id}'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
